03d3fc820d2a8016c03a62d3c3d4809f31325912
mahmoud ewis <mahmoudewis.ai@gmail.com>
2025-05-09 02:38:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def retrain_model():
                 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
                 # تدريب النموذج
-                model = RandomForestClassifier(n_estimators=100, random_state=42)
+                model = RandomForestClassifier(n_estimators=50, random_state=42)
                 model.fit(X_train, y_train)
 
                 # حساب التقييمات
