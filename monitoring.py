a24093fc1e00fc4f05c50ae0c5636e31096f3f05
mahmoud ewis <mahmoudewis.ai@gmail.com>
2025-05-08 14:42:50 +0300

    monitoring.py

--- monitoring.py
+++ monitoring.py
@@ -1,8 +1,9 @@
-from evidently.report import Report
-from evidently.metrics import DataDriftPreset
 import pandas as pd
 
 def generate_drift_report(reference_data, current_data, output_path="data/drift_report.html"):
+    from evidently.report import Report
+    from evidently.metrics import DataDriftPreset
+
     report = Report(metrics=[DataDriftPreset()])
     report.run(reference_data=reference_data, current_data=current_data)
     report.save_html(output_path)
