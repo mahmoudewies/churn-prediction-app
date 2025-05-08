7dd2caff0cf7edce69c1851721c85d482e5bcc08
mahmoud ewis <mahmoudewis.ai@gmail.com>
2025-05-08 14:53:43 +0300

    monitoring.py

--- monitoring.py
+++ monitoring.py
@@ -1,7 +1,13 @@
 def generate_drift_report(reference_data, current_data, output_path="data/drift_report.html"):
-    from evidently.report import Report
-    from evidently.metrics import DataDriftPreset
-    report = Report(metrics=[DataDriftPreset()])
-    report.run(reference_data=reference_data, current_data=current_data)
-    report.save_html(output_path)
-    return output_path
+    try:
+        from evidently.report import Report
+        from evidently.metrics import DataDriftPreset
+
+        report = Report(metrics=[DataDriftPreset()])
+        report.run(reference_data=reference_data, current_data=current_data)
+        report.save_html(output_path)
+        return output_path
+    except ImportError as e:
+        st.error(f"Error while importing libraries: {e}")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
